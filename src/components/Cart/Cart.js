9da2dfdd2206f1f5a713d281f998fdaeaacd0153
Dark-Fahim <itsmunna1066@gmail.com>
2022-08-16 00:05:23 +0600

    cart updated to local storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,36 @@
 import React from 'react';
+// import { deleteShoppingCart } from '../../utilities/fakedb';
 import './Cart.css'
 
-const Cart = ({cart}) => {
+
+const Cart = ({cart,deleteShoppingCartBtn}) => {
+    // console.log(cart);
+    let total =0;
+    let  totalShipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity
+        total = total + product.price * product.quantity
+        totalShipping =totalShipping + product.shipping
+    }
+    let tax  = (total * .1).toFixed(2);
+    tax = parseFloat(tax)
+    const totalPrice = totalShipping + total + tax;
+    
+
+
+
     return (
-        <div>
-            <p className='summery'>Order Summery</p>
+        <div className='carts'> 
+            <p className='summery-header'>Order Summery</p>
                 <div className='summery-info'>
-                    <p>Selected Items: {cart.length}</p>
-                    <p>Total Price: $0</p>
-                    <p>Total Shipping Charge: $0</p>
-                    <p>Tax: $0</p>
-                    <h4>Grand Total: $0</h4>
+                    <p>Selected Items: {quantity}</p>
+                    <p>Total Price: ${total}</p>
+                    <p>Total Shipping Charge: ${totalShipping}</p>
+                    <p>Tax: ${tax}</p>
+                    <h4>Grand Total: ${totalPrice.toFixed(2)}</h4>
                 </div>
-                <button className='clear-cart-btn'>
+                <button onClick={deleteShoppingCartBtn} className='clear-cart-btn'>
                     <p>Clear Cart</p>
                 </button>
                 <button className='review-cart-btn'>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, deleteShoppingCart, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -14,13 +15,46 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[])
 
+    useEffect(()=>{
+        const storedCart = getStoredCart();
+        const savedCart = []
+        for(const id in storedCart){
+            const addedProduct = products.find(pd=> pd.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct)
+            }
+        }
+        setCart(savedCart)
+    },[products])
+
 
     const handleAddToCart = (product)=>{
-        console.log(product);
-        const newCart = [...cart, product];
+        let newCart = []
+        const exist = cart.find(pd => pd.id === product.id)
+        if(!exist){
+            product.quantity = 1;
+            newCart =  [...cart, product]
+        }
+        else{
+            const rest = cart.filter(pd => pd.id !== product.id)
+            exist.quantity = exist.quantity + 1;
+            newCart = [...rest, exist]
+        }
+
+
+
+       
         setCart(newCart);
+        addToDb(product.id)
     }
 
+    const deleteShoppingCartBtn = ()=>{
+        deleteShoppingCart()
+        const newCart = []
+        setCart(newCart)
+    }
 
     return (
         <div className='shop-container'>
@@ -34,7 +68,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart} deleteShoppingCartBtn={deleteShoppingCartBtn}></Cart>
             </div>
         </div>
     );
